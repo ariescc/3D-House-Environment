bd0294c88366a846554e014b092c216130f81a63
Zeyad Atef <zeatef@gmail.com>
2016-06-22 04:18:55 +0200

    Draw Axes and Ground (Needs Revising)

--- 3D House Environment/3D House Environment/main.cpp
+++ 3D House Environment/3D House Environment/main.cpp
@@ -4,16 +4,65 @@
 #include <OpenGl/glu.h>
 #include <GLUT/glut.h>
 
+void axis(double length)
+{
+    // draw a z-axis
+    glPushMatrix();
+    glBegin(GL_LINES);
+    glVertex3d(0, 0, 0);
+    glVertex3d(0,0,length); // along the z-axis
+    glEnd();
+    glTranslated(0, 0,length -0.2);
+    glutWireCone(0.04, 0.2, 12, 9);
+    glPopMatrix();
+    
+}
+
+void Initialize() {
+    glMatrixMode(GL_PROJECTION); // set the view volume shape
+    glLoadIdentity();
+    glOrtho(-2.0*10/6.0, 2.0*10/6.0, -2.0, 2.0, 0.1, 100);
+    glMatrixMode(GL_MODELVIEW); // position and aim the camera
+    glLoadIdentity();
+    gluLookAt(1.0,1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glTranslatef(-1.5f, 1.0f, .75f);
+    glPushMatrix();
+    //draw z-axis in red
+    glColor3d(1,0,0);
+    axis(2.0);
+    //draw x-axis in green
+    glColor3d(0,1,0);
+    glRotated(90, 0,1.0, 0);
+    axis(4.0);
+    //draw y-axis in blue
+    glColor3d(0,0,1);
+    glRotated(-90.0, 1, 0, 0);
+    axis(1.0);
+    glPopMatrix();
+}
+
+void DrawGround() {
+    glPushMatrix();
+    glBegin(GL_QUADS);
+    glColor3d(0.0, .390625, 0.0);
+    glVertex3d(0.1, 0, 0.1);
+    glVertex3d(3.0, 0, 0.1);
+    glVertex3d(3.0, 0, 2.0);
+    glVertex3d(0.1, 0, 2.0);
+    glEnd();
+    glPopMatrix();
+    
+}
+
 void keyboard(unsigned char key, int x, int y) {
     //code goes here...
     //glutPostRedisplay();
 }
 
 void Display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glPushMatrix();
-    //code goes here...
-    glPopMatrix();
+    Initialize();
+    DrawGround();
     glFlush();
 }
 
